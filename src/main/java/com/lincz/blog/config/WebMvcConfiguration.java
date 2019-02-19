de5c936508d00f985514fa563acfbcdc9446a578
taylor <980415639@qq.com>
2019-02-19 21:55:20 +0800

    Blog

--- src/main/java/com/lincz/blog/config/WebMvcConfiguration.java
+++ src/main/java/com/lincz/blog/config/WebMvcConfiguration.java
@@ -1,11 +1,8 @@
 package com.lincz.blog.config;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
